3ac5282445f8d54e234c2b51094088fc88ceb45f
Danny <dhcx03@gmail.com>
2024-05-07 22:19:28 -0600

    Numbers, Arithmetic Operators and Functions

---
+++ 03.Numbers-ArithmeticOperators-Functions.py
@@ -0,0 +1,29 @@
+import math
+#Arithmetic Operators
+"""
++ Suma
+- Resta
+* Multiplicacion
+/ Division
+// Cociente
+% Mod
+** Exponente
+"""
+
+print(2+3) #5
+print(3-2) #1
+print(3*2) #6
+print(6/2) #3.0
+print(1 // 3)
+print(1 % 3)
+print(2**3) #8
+
+#Functions with numbers
+print("\nFunctions\n")
+print(round(1.3)) # 1
+print(round(1.10)) # 1
+print(abs(-77)) # 77
+print(math.ceil(1.3)) # 2
+print(math.floor(1.3)) # 1
+print(math.pow(10, 3)) # 1000.0
+print(math.sqrt(9)) # 3.0
