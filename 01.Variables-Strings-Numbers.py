23025f6b3f4847134fb6055d2737afc6846ec010
Danny <dhcx03@gmail.com>
2024-05-07 21:30:56 -0600

    Type of Variables ,Strings and Numbers

---
+++ 01.Variables-Strings-Numbers.py
@@ -0,0 +1,42 @@
+# Hi, this is a single line comment.
+
+#Variables can be set just by writing the name of the variable, then with the = symbol the value of the variable
+
+variable_name = "Underscore_Case"
+variableName = "lowerCamelCase"
+VariableName = "UpperCamelCase"
+
+print("Hi This is a comment printed on screen")
+print(variable_name, variableName, VariableName)
+
+"""
+This is a comment
+written in
+more than just one line
+in Python
+"""
+
+#Strings
+
+CourseName = "Python"
+
+CourseDescription = """
+Python Essentials,
+In this material we will learn the basics of Python
+"""
+
+print("Name of the course: ", CourseName)
+print("Course Description: ", CourseDescription)
+
+#Numbers
+
+Number = 2
+Decimal = 2.5
+Number_Complex = 2 + 2j
+
+
+print(Number_Complex)
+print(type(CourseName))
+print(type(Number))
+print(type(Decimal))
+print(type(Number_Complex))
